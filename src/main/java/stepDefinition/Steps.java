e70d9bd0c2f01966222b2a79727ba0cbfc8679bd
vigneshvky45 <vigneshvky45@gmail.com>
2024-03-28 08:16:48 +0530

    New file

--- src/main/java/stepDefinition/Steps.java
+++ src/main/java/stepDefinition/Steps.java
@@ -6,13 +6,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.base.CaseFormat;
 
 import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 	
 public class Steps {
 	
@@ -36,19 +41,81 @@ public class Steps {
 	}
 	
 	@Given("I am logged in as {string}")
-	public void login(String user) {
-		driver.findElement(By.xpath("//input[@class='input-text' and @name='username']")).sendKeys("vramanathan@sympdebug");
-		driver.findElement(By.xpath("//input[@class='input-password' and @name='password']")).sendKeys("Vkyking@123a");
+	public void login(String user) 
+	{
+		String username = "";
+		String password = "iopex@123";
+		if(user == "vignesh") {
+			username = "vramanathan@sympdebug";
+			password = "Vkyking@123a";
+		} else if (user == "Manager_Home"){
+			username = "home@iopexadmin.com";
+		} else {
+			username = "testmanager@iopexadmin.com";
+		}
+		driver.findElement(By.xpath("//input[@class='input-text' and @name='username']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@class='input-password' and @name='password']")).sendKeys(password);
 		driver.findElement(By.xpath("//input[@type='submit' and @value='Sign In']")).click();
 	}
 
 	@Then("I will see {string}")
-	public void iWillSee(String text) {
-		WebElement textByXapth = driver.findElement(By.xpath("//span[@class='navtext' and text()='"+text+"']"));
-		String actual = textByXapth.getText();
+	public void iWillSee(String text) 
+	{
+		WebElement textByXapth = driver.findElement(By.xpath("//*[text()='"+text+"']"));
+		String actual = "";
+		try {
+			actual = textByXapth.getText();
+		} catch(Exception $e) {
+			wait.until(ExpectedConditions.visibilityOf(textByXapth));
+			actual = textByXapth.getText();
+		}
 		if (text.equalsIgnoreCase(actual)) {
 			System.out.println(actual);
 		}
-		System.out.println("GitPushTest");
+	}
+	
+	@When("I navigate to {string}")
+	public void iNavigateTo(String path)
+	{
+		String navMenu[] = path.split(">");
+		WebElement mainMenu = driver.findElement(By.xpath("//a[@title='Students']//i[@id='toggler_"+navMenu[0].toLowerCase()+"']"));
+		mainMenu.click();
+//		WebElement subMenu = driver.findElement(By.xpath("//a[@class='navitem hi no-children']//span[@class='navtext' and text()='"+navMenu[1]+"']"));
+//		try {
+//			subMenu.click();
+//		} catch(Exception $e) {
+//			wait.until(ExpectedConditions.elementToBeClickable(subMenu));
+//			subMenu.click();
+//		}
+	}
+	
+	@And("I search with {string}")
+	public void searchWith(String value) 
+	{
+		WebElement keywordFilter =  driver.findElement(By.xpath("//input[@id='studentfilters_keywords_' and @type='text']"));
+		keywordFilter.clear();
+		keywordFilter.sendKeys(value);
+		driver.findElement(By.xpath("//input [@type='submit' and @value='apply search']")).click();
+	}
+	
+	@And("I click {string} Link")
+	public void clickLink(String link)
+	{
+		driver.findElement(By.linkText(link)).click();
+	}
+	
+	@And("I open {string} tab")
+	public void iOpenTab(String tab)
+	{
+		driver.findElement(By.xpath("//div[@class='tab_text' and normalize-space(text())='"+tab+"']")).click();
+	}
+	
+	@And("I impersonateAs {string}")
+	public void iImpersonateAs(String user)
+	{
+		this.clickLink(user);
+		this.iOpenTab("Login As");
+		driver.switchTo().frame("loginas_frame");
+		this.clickLink("Open in a separate window");
 	}
 }
--- src/test/java/Features/simple.feature
+++ src/test/java/Features/simple.feature
@@ -1,6 +1,35 @@
 Feature: Test simple web launch
 
+@FULL-CSM
 Scenario: Verify to launch simple site
 Given I am logged in as "vignesh"
 Then I will see "Home"
 #But I will not see "Username"
+When I navigate to "Students>Students"
+Then I will see "Keywords"
+And I search with "AAAStudent"
+And I impersonateAs "AAAStudent"
+#And I click "AAAStudent" Link
+#And I open "Login As" tab
+
+Scenario: Verify to launch simple site with home
+Given I am logged in as "Manager_Home"
+Then I will see "Home"
+#But I will not see "Username"
+When I navigate to "Students>Students"
+Then I will see "Keywords"
+And I search with "AAAStudent"
+And I impersonateAs "AAAStudent"
+#And I click "AAAStudent" Link
+#And I open "Login As" tab
+
+Scenario: Verify to launch simple site with manager
+Given I am logged in as "Manager"
+Then I will see "Home"
+#But I will not see "Username"
+When I navigate to "Students>Students"
+Then I will see "Keywords"
+And I search with "AAAStudent"
+And I impersonateAs "AAAStudent"
+#And I click "AAAStudent" Link
+#And I open "Login As" tab
